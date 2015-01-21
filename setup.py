5b93a0fded40f9b3b284d2f604f4648d1f29c9da
津崎　豪宏 <takehiro.tsu@gmail.com>
2015-01-21 09:55:57 +0900

    setup.py修正

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@
 from distutils.core import setup
 
 setup(
-		name='Bodies',
-		version='1.0.0'
-		description='Bodies written by Python and PyOpenGL',
+                name='Bodies',
+                version='1.0.0',
+                description='Bodies written by Python and PyOpenGL',
 		url='http://www.cc.kyoto-su.ac.jp/~atsushi/',
 		author='AOKI Atsushi',
 		author_email='atsushi@cc.kyoto-su.ac.jp',
 		license='GNU GENERAL PUBLIC LICENSE',
 		long_description='ドラゴン・スズメバチ・うさぎ・ペンギン・鬼・赤ちゃんを３次元描画がする',
-		platforms='OS X (10.10')
+		platforms='OS X (10.10)',
 		packages=['jp'],
-	)
+        )
