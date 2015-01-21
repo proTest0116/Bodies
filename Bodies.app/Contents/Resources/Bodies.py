5bebf6c1c2738b747548393b7f7dae3e26188af5
watari <WATARI@WATARI.local>
2015-01-22 01:44:45 +0900

    WATARI
    .appが動かなさそうだったため、修正

---
+++ Bodies.app/Contents/Resources/Bodies.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+# -*- codinf: utf-8 -*-
+
+import sys
+
+import jp.ac.kyoto_su.cse.wz.Bodies.Example as Example
+
+if __name__ == '__main__':
+    sys.exit(Example.main())
---
+++ Bodies.app/Contents/Resources/setup.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+                name='Bodies',
+                version='1.0.0',
+                description='Bodies written by Python and PyOpenGL',
+		url='http://www.cc.kyoto-su.ac.jp/~atsushi/',
+		author='AOKI Atsushi',
+		author_email='atsushi@cc.kyoto-su.ac.jp',
+		license='GNU GENERAL PUBLIC LICENSE',
+		long_description='ドラゴン・スズメバチ・うさぎ・ペンギン・鬼・赤ちゃんを３次元描画がする',
+		platforms='OS X (10.10)',
+		packages=['jp'],
+        )
